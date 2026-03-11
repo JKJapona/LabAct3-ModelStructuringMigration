67a8c39c63d66ebbcd9fe8cc29e2310601199644
JKJapona <j.japona.558573@umindanao.edu.ph>
2026-03-11 16:09:23 +0800

    Feat: Implemented Database Migration

---
+++ database/migrations/2026_03_11_071814_create_countries_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+    Schema::create('countries', function (Blueprint $table) {
+        $table->char('country_id', 2)->primary(); // Primary Key as Char(2)
+        $table->string('country_name', 40)->nullable();
+        $table->foreignId('region_id')->constrained('regions', 'region_id');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
---
+++ database/migrations/2026_03_11_071815_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+    Schema::create('employees', function (Blueprint $table) {
+        $table->id('employee_id');
+        $table->string('first_name', 20)->nullable();
+        $table->string('last_name', 25);
+        $table->string('email', 100);
+        $table->string('phone_number', 20)->nullable();
+        $table->date('hire_date');
+        $table->foreignId('job_id')->constrained('jobs', 'job_id');
+        $table->decimal('salary', 8, 2);
+        $table->unsignedBigInteger('manager_id')->nullable();
+        $table->foreignId('department_id')->constrained('departments', 'department_id');
+        $table->timestamps();
+
+        // Self-referencing relationship
+        $table->foreign('manager_id')->references('employee_id')->on('employees');
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
